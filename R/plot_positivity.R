1047eae80fa8fef227a4866812f274fefa0f18cc
RuBBiT-hj <jameslilig@gmail.com>
2023-08-28 23:44:56 -0400

    Update plot_positivity
    Update plot_positivity to work on bayeshaz class object

--- R/plot_positivity.R
+++ R/plot_positivity.R
@@ -5,17 +5,29 @@
 #' If the assumption is met, the mirrored histogram should look similar.
 #' If there is an obvious shift in the distribution of histograms, the assumption is likely violated.
 #' 
-#' @param d the data frame in survival format, i.e. processed by Surv function
-#' @param formula the formula for the logistic regression for propensity score (a formula object in this version)
-#' @param treatment the treatment variable name
-
+#' @param bayeshaz_object an object of the class `bayeshaz` created by the `bayeshaz()` function
+#' @param formula an optional formula variable used in fitting propensity score model
+#' @param breaks the parameter set for breaks, default is `"Scott"`
 #' @examples
 #' # example demo
 ## usethis namespace: start
 ## usethis namespace: end
 #' @export
 
-plot_positivity = function(d, formula, treatment){
+plot_positivity = function(bayeshaz_object, formula = NULL, breaks="Scott"){
+  
+  # extract
+  d = bayeshaz_object$data
+  covariates = bayeshaz_object$covariates
+  treatment = bayeshaz_object$treatment
+  
+  # remove the terms and interaction terms related to treatment
+  covariates = covariates[!(grepl(treatment, covariates, fixed = TRUE))]
+  
+  if (is.null(formula)) formula = formula(
+    paste0(treatment, "~", paste0(covariates, collapse = "+"))
+    )
+  
   # Fit the logistic regression
   pos_check <- glm(formula, family = binomial(link = "logit"),
                    data = d)
@@ -28,22 +40,24 @@ plot_positivity = function(d, formula, treatment){
   score_2 <- predict(pos_check, newdata = d[d_trt == trt_2, ], type = "response")
   
   # Get parameters for histogram
-  h_1 <- hist(score_1, breaks = "Scott", probability = T)
-  h_2 <- hist(score_2, breaks = "Scott", probability = T)
+  h_1 <- hist(score_1, breaks = breaks, probability = T)
+  h_2 <- hist(score_2, breaks = breaks, probability = T)
   
-  x_min = min(c(score_1, score_2))*0.98
-  x_max = min(1, max(c(score_1, score_2))*1.02) # not exceed 1
+  x_min = min(c(score_1, score_2))*0.9
+  x_max = min(1, max(c(score_1, score_2))*1.1) # not exceed 1
   y_max = 1.02 * max(c(h_1$density, h_2$density))
+  
+  message(paste0("The above is ", trt_1, " and the below is ", trt_2))
   
   # Plot
   par(mfrow=c(2,1), mar=c(0,5,3,3))
-  hist(score_1 , main="Positivity Assumption Checking" , ylab="Density", xlab="", 
+  hist(score_1 , main="Positivity Overlap" , ylab="Density", xlab="", 
        ylim=c(0, y_max), xlim = c(x_min, x_max), 
-       xaxt="n", las=1 , col="slateblue1", breaks="Scott", probability = T)
+       xaxt="n", las=1 , col="slateblue1", breaks=breaks, probability = T)
   par(mar=c(5,5,0,3))
   hist(score_2 , main="" , ylab="Density", xlab="Propensity Score", 
        ylim=c(y_max, 0),  xlim = c(x_min, x_max),
-       las=1 , col="tomato3"  , breaks = "Scott", probability = T)
+       las=1 , col="tomato3", breaks = breaks, probability = T)
   # reset
   par(mfrow=c(1,1),
       mar = c(5.1, 4.1, 4.1, 2.1),
